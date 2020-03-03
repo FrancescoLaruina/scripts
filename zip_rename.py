18954a31cb4778b2b3d0bd09f74351dad6651213
Francesco Laruina <cescomail@gmail.com>
2020-03-03 11:02:08 +0100

    Added prefix definition for the extracted files

--- zip_rename.py
+++ zip_rename.py
@@ -7,8 +7,11 @@ import zipfile
 def build_parser():
     """ Build a parser for the command line interface """
     parser = argparse.ArgumentParser(description='A zip renamer utility')
-    parser.add_argument('filename', help='The zip file path')
-    parser.add_argument('--prefix', help='A prefix for the extracted files')
+    parser.add_argument('filename',
+                        help='The zip file path')
+    parser.add_argument('--prefix',
+                        help='A prefix for the extracted files',
+                        default='file')
     return parser
 def extract_rename(zip_path):
     """ Extract and rename the file contained in the zip archive.
@@ -21,7 +24,7 @@ def extract_rename(zip_path):
     os.mkdir(unzip_folder)
     zip_file = zipfile.ZipFile(zip_path, mode='r')
     for idx, file_ in enumerate(zip_file.filelist):
-        file_.filename = 'file_{}'.format(idx)
+        file_.filename = '{}_{}'.format(ARGS.prefix, idx)
         zip_file.extract(file_, path=unzip_folder)
 if __name__ == "__main__":
     ARGS = build_parser().parse_args()
